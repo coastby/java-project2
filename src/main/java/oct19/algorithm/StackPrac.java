537c5aa662518bfeb09e33256851f5325acdf20b
JO YEJI <whdpwl2@gmail.com>
2022-10-19 10:46:04 +0900

    Stack pop() 구현 테스트코드 작성 및 통과

--- src/main/java/oct19/algorithm/StackPrac.java
+++ src/main/java/oct19/algorithm/StackPrac.java
@@ -1,7 +1,5 @@
 package oct19.algorithm;
 
-import java.util.Stack;
-
 public class StackPrac {
     private int[] arr;
     private int pointer = 0;
@@ -14,10 +12,17 @@ public class StackPrac {
     public StackPrac(int size) {
         arr  = new int[size];
     }
-    public int[] getStack(){
+    public int[] getArr(){
         return this.arr;
     }
     public void push(int value){
         arr[pointer++] = value;
+    }
+
+    public int pop(){
+        //push를 하면 pointer가 마지막 값의 위의 인덱스를 가리키고 있어서 pointer-1이 현재의 값일 것이다.
+        int p = arr[pointer-1];
+        --pointer;
+        return p;
     }
 }
--- src/test/java/oct19/algorithm/StackPracTest.java
+++ src/test/java/oct19/algorithm/StackPracTest.java
@@ -1,9 +1,8 @@
 package oct19.algorithm;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class StackPracTest {
     @Test
@@ -12,10 +11,24 @@ class StackPracTest {
         stackPrac.push(0);
         stackPrac.push(2);
 
-        int[] result = stackPrac.getStack();
+        int[] result = stackPrac.getArr();
 
         Assertions.assertEquals(2, result[1]);
         Assertions.assertEquals(0, result[0]);
+    }
+
+    @Test
+    @DisplayName("pop(), push() test")
+    public void pushAndPop(){
+        StackPrac sp = new StackPrac();
+        sp.push(100);
+        sp.push(200);
+
+        Assertions.assertEquals(100, sp.getArr()[0]);
+        Assertions.assertEquals(200, sp.getArr()[1]);
+
+        int a = sp.pop();
+        Assertions.assertEquals(200, a);
     }
 
 }
