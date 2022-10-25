a8278e9fba5d2dc9e1b4568288335a8bd18a74c5
JO YEJI <whdpwl2@gmail.com>
2022-10-25 10:23:14 +0900

    HashTable hash(), insert(), search()

---
+++ src/main/java/oct25/HashPrac.java
@@ -0,0 +1,17 @@
+package oct25;
+
+public class HashPrac {
+    public int hash(String key) {
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % 90;
+    }
+
+    public static void main(String[] args) {
+        HashPrac hp = new HashPrac();
+        int hashcode = hp.hash(("YejiJo"));
+        System.out.println(hashcode);
+    }
+}
