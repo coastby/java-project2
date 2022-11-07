ebceb90398ece19dba6ddde83bebb77051fca1db
JO YEJI <whdpwl2@gmail.com>
2022-11-07 10:30:41 +0900

    [프로그래머스] 나누어 떨어지는 숫자 배열 (우선순위큐 이용)

--- src/main/java/nov7/DividableArray.java
+++ src/main/java/nov7/DividableArray.java
@@ -3,26 +3,28 @@ package nov7;
 import java.util.PriorityQueue;
 
 //프로그래머스 나누어 떨어지는 숫자 배열
-public class DivadableArray {
+public class DividableArray {
     public int[] solution(int[] arr, int divisor) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int i : arr) {
-            if(i%divisor==0){
-                queue.add(i);
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%divisor == 0){
+                queue.add(arr[i]);
             }
         }
-        if(queue.size()==0){
-            return new int[]{-1};
+        int N = queue.size();
+        if(N==0){
+            return new int[] {-1};
         }
-        int[] answer = new int[queue.size()];
-        for(int i = 0; i < queue.size(); i++){
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
             answer[i] = queue.poll();
         }
+
         return answer;
     }
 
     public static void main(String[] args) {
-        DivadableArray da = new DivadableArray();
+        DividableArray da = new DividableArray();
         System.out.println(da.solution(new int[]{5, 9, 7, 10}, 5));
         System.out.println(da.solution(new int[]{2, 36, 1, 3}, 1));
         System.out.println(da.solution(new int[]{3,2,6}, 10));
