a60f7861047d3e701cc267dfbb3b690f92057d7d
JO YEJI <whdpwl2@gmail.com>
2022-11-14 10:49:43 +0900

    [에라토스테네스의체]시간초과 나와서 리팩토링

--- src/main/java/nov14/EraPrac.java
+++ src/main/java/nov14/EraPrac.java
@@ -7,18 +7,16 @@ public class EraPrac {
     public int EraChe(int n) {
         //boolean 배열 생성
         boolean[] nums = new boolean[n+1];
-        //true로 채우기
-        Arrays.fill(nums, true);
         for (int i = 2; i*i <= n; i++) {
-            for (int j = 2; j <= n ; j++) {
-                if(nums[i] &&(j%i == 0 && j != i)){         //irk 지워지지 않았을 때, index가 i가 아닌 i의 배수이면
-                    nums[j] = false;                        //지운다
+            for (int j =i*2; j <= n ; j++) {      //j를 i*2부터 시작하도록 리팩토링
+                if(!nums[i] &&(j%i == 0)){         //i가 지워지지 않았을 때, index가 i가 아닌 i의 배수이면
+                    nums[j] = true;                        //지운다
                 }
             }
         }
         int cnt = 0;                        //갯수세기
         for (int i = 2; i < n+1; i++) {     //배열을 돌면서 true인 인덱스 출력
-            if (nums[i]) {
+            if (!nums[i]) {
                 System.out.print(i+" ");
                 cnt++;
             }
@@ -33,7 +31,7 @@ public class EraPrac {
             nums[i-2] = i;
         }
         //2. 2를 제외한 2의 배수 지우기
-        for (int i = 0; i < nums.length; i++){
+        for (int i = 2; i < nums.length; i++){
             if(nums[i] % 2 == 0 && nums[i] != 2){
                 nums[i] = 0;
             }
