f2227ede912581cf7384f40904b2d3a44f1d70b7
JO YEJI <whdpwl2@gmail.com>
2022-11-18 10:03:15 +0900

    [Programmers] 행렬의 덧셈 - 이쁘게

--- src/main/java/november/nov18/ArrayPlus.java
+++ src/main/java/november/nov18/ArrayPlus.java
@@ -4,9 +4,9 @@ package november.nov18;
 public class ArrayPlus {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = arr1;
-        for(int j = 0; j < arr1.length; j++){
-            for(int i = 0; i < arr1[j].length; i++){
-                answer[j][i] = arr1[j][i] + arr2[j][i];
+        for(int j = 0; j < answer.length; j++){
+            for(int i = 0; i < answer[j].length; i++){
+                answer[j][i] += arr2[j][i];
             }
         }
         return answer;
