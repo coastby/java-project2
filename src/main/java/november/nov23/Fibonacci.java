5c9bd9cbaf696a0866f0859a48cdee9199fa9467
JO YEJI <whdpwl2@gmail.com>
2022-11-23 10:40:05 +0900

    [codeup] 피보나치 재귀 안 쓰고 구현

--- src/main/java/november/nov23/Fibonacci.java
+++ src/main/java/november/nov23/Fibonacci.java
@@ -10,10 +10,20 @@ public class Fibonacci {
         }
         return fibo(--num)+fibo(--num);
     }
+    public int fiboBottomUp(int num){
+        int[] arr = new int[num+1];
+        arr[1] = 1;
+        arr[2] = 1;
+        for (int i = 3; i <= num; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return  arr[num];
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Fibonacci f = new Fibonacci();
         System.out.println(f.fibo(sc.nextInt()));
+        System.out.println(f.fiboBottomUp(sc.nextInt()));
     }
 
 }
