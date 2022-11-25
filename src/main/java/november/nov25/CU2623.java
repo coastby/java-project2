4811f8b49892b5ed32002d2b64f3b9da837f8ea6
JO YEJI <whdpwl2@gmail.com>
2022-11-25 09:41:41 +0900

    [codeup] 반복문으로 최대공약수 구하기

--- src/main/java/november/nov25/CU2623.java
+++ src/main/java/november/nov25/CU2623.java
@@ -6,20 +6,30 @@ import java.util.Scanner;
 public class CU2623 {
     public int gcd(int a, int b){
         if (a < b){
-            int tmp = a;
-            a = b;
-            b = tmp;
+//            int tmp = a;
+//            a = b;
+//            b = tmp;
+            return gcd(a, b - a);
         } else if (a == b) {
             return a;
         }
         return gcd(a - b, b);
-
+    }
+    public int gcd2(int a, int b){
+        while(a != b){
+            if (a < b){
+                b -= a;
+            } else {
+                a -= b;
+            }
+        }
+        return a;
     }
 
 
     public static void main(String[] args) {
         CU2623 c = new CU2623();
         Scanner sc = new Scanner(System.in);
-        System.out.println(c.gcd(sc.nextInt(), sc.nextInt()));
+        System.out.println(c.gcd2(sc.nextInt(), sc.nextInt()));
     }
 }
