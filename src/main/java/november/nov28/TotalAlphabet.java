da32a8f24eb9a3ebcbaa20757bd255e558d4cd4b
JO YEJI <whdpwl2@gmail.com>
2022-11-28 09:38:19 +0900

    (재귀) AA-ZZ 출력하기

--- src/main/java/november/nov28/TotalAlphabet.java
+++ src/main/java/november/nov28/TotalAlphabet.java
@@ -15,15 +15,21 @@ public class TotalAlphabet {
             }
         }
     }
-    public void aToZ2(char ch){
+    public void aToZ2(char ch, char fix){
         if (ch == 'Z'+1) return;
-        System.out.println(ch);
-        aToZ2(++ch);
+        System.out.println(fix+""+ch);
+        aToZ2(++ch, fix);
     }
+    public void aToZ3(char ch){
+        if (ch == 'Z'+1) return;
+        aToZ2('A', ch);
+        aToZ3(++ch);
+    }
+
 
     public static void main(String[] args) {
         TotalAlphabet a = new TotalAlphabet();
-        a.aToZ2('A');
+        a.aToZ3('A');
     }
 
 
