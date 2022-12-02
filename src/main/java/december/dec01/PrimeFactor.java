d7f13fd347740647fa31a7302dcc6e0ada1b8cd8
JO YEJI <whdpwl2@gmail.com>
2022-12-02 09:14:26 +0900

    [codeup] 2203 소인수 구하기 (통과)

--- src/main/java/december/dec01/PrimeFactor.java
+++ src/main/java/december/dec01/PrimeFactor.java
@@ -2,25 +2,28 @@ package december.dec01;
 
 import java.util.Scanner;
 
+//[codeup] 2203 소인수 구하기
 public class PrimeFactor {
-    public static boolean isPrime(int n){
-        boolean result = true;
-        for (int i = 2; i*i < n; i++) {
+    public static boolean isPrime(long n){
+        for (long i = 2; i*i <= n; i++) {
             if(n%i == 0){
-                result = false;
-                break;
+                return false;
             }
         }
-
-        return result;
+        return true;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long num = sc.nextLong();
         long result = 0;
-        for (int i = 2; i <= num; i++) {
-            if(isPrime(i) && num%i == 0){
-                result = i;
+        for (long i = 1; i*i <= num; i++) {
+            if(num%i == 0){
+                if(isPrime(i)){
+                    result = Math.max(i, result);
+                }
+                if(isPrime(num/i)){
+                    result = Math.max(result, num/i);
+                }
             }
         }
         System.out.println(result);
